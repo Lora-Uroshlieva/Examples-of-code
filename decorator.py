59ecb264be01c57f92eed05f20279e71dbfa0f51
Lora-Uroshlieva <lora.strizhakova@gmail.com>
2017-01-17 21:40:19 +0200

    example was added

--- decorator.py
+++ decorator.py
@@ -11,3 +11,20 @@ def multiple(arg1, arg2):
     return arg1*arg2
 
 multiple(3, 6)
+
+
+# decorator without arguments
+def my_decorator(function_for_decor):
+    def wrapper():
+        print('text1')
+        function_for_decor()
+        print('text2')
+    return wrapper
+
+@my_decorator
+def func_1():
+    print('between text')
+
+func_1()
+
+print(func_1.__doc__)
